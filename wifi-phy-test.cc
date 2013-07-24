d0d1d55da253ef1785a3488e2ae2cb15049d6e25
Takashi SASAKI <takashi316@gmail.com>
2013-07-24 12:33:58 +0900

    wifi-phy-test.cc was slimmed

--- wifi-phy-test.cc
+++ wifi-phy-test.cc
@@ -36,98 +36,6 @@
 
 using namespace ns3;
 
-class PsrExperiment
-{
-public:
-  struct Input
-  {
-    Input ();
-    double distance;
-    std::string txMode;
-    uint8_t txPowerLevel;
-    uint32_t packetSize;
-    uint32_t nPackets;
-  };
-  struct Output
-  {
-    uint32_t received;
-  };
-  PsrExperiment ();
-
-  struct PsrExperiment::Output Run (struct PsrExperiment::Input input);
-
-private:
-  void Send (void);
-  void Receive (Ptr<Packet> p, double snr, WifiMode mode, enum WifiPreamble preamble);
-  Ptr<WifiPhy> m_tx;
-  struct Input m_input;
-  struct Output m_output;
-};
-
-void
-PsrExperiment::Send (void)
-{
-  Ptr<Packet> p = Create<Packet> (m_input.packetSize);
-  WifiMode mode = WifiMode (m_input.txMode);
-  m_tx->SendPacket (p, mode, WIFI_PREAMBLE_SHORT, m_input.txPowerLevel);
-}
-
-void
-PsrExperiment::Receive (Ptr<Packet> p, double snr, WifiMode mode, enum WifiPreamble preamble)
-{
-  m_output.received++;
-}
-
-PsrExperiment::PsrExperiment ()
-{
-}
-PsrExperiment::Input::Input ()
-  : distance (5.0),
-    txMode ("OfdmRate6Mbps"),
-    txPowerLevel (0),
-    packetSize (2304),
-    nPackets (400)
-{
-}
-
-struct PsrExperiment::Output
-PsrExperiment::Run (struct PsrExperiment::Input input)
-{
-  m_output.received = 0;
-  m_input = input;
-
-  Ptr<MobilityModel> posTx = CreateObject<ConstantPositionMobilityModel> ();
-  posTx->SetPosition (Vector (0.0, 0.0, 0.0));
-  Ptr<MobilityModel> posRx = CreateObject<ConstantPositionMobilityModel> ();
-  posRx->SetPosition (Vector (m_input.distance, 0.0, 0.0));
-
-  Ptr<YansWifiChannel> channel = CreateObject<YansWifiChannel> ();
-  channel->SetPropagationDelayModel (CreateObject<ConstantSpeedPropagationDelayModel> ());
-  Ptr<LogDistancePropagationLossModel> log = CreateObject<LogDistancePropagationLossModel> ();
-  channel->SetPropagationLossModel (log);
-
-  Ptr<YansWifiPhy> tx = CreateObject<YansWifiPhy> ();
-  Ptr<YansWifiPhy> rx = CreateObject<YansWifiPhy> ();
-  Ptr<ErrorRateModel> error = CreateObject<YansErrorRateModel> ();
-  tx->SetErrorRateModel (error);
-  rx->SetErrorRateModel (error);
-  tx->SetChannel (channel);
-  rx->SetChannel (channel);
-  tx->SetMobility (posTx);
-  rx->SetMobility (posRx);
-
-  rx->SetReceiveOkCallback (MakeCallback (&PsrExperiment::Receive, this));
-
-  for (uint32_t i = 0; i < m_input.nPackets; ++i)
-    {
-      Simulator::Schedule (Seconds (i), &PsrExperiment::Send, this);
-    }
-  m_tx = tx;
-  Simulator::Run ();
-  Simulator::Destroy();
-  return m_output;
-}
-
 
 class CollisionExperiment
 {
@@ -202,6 +110,7 @@ CollisionExperiment::Receive (Ptr<Packet> p, double snr, WifiMode mode, enum Wif
 CollisionExperiment::CollisionExperiment ()
 {
 }
+
 CollisionExperiment::Input::Input ()
   : interval (MicroSeconds (0)),
     xA (-5),
@@ -272,123 +181,7 @@ CollisionExperiment::Run (struct CollisionExperiment::Input input)
 }
 
 
-static void PrintPsr (int argc, char *argv[])
-{
-  PsrExperiment experiment;
-  struct PsrExperiment::Input input;
-
-  CommandLine cmd;
-  cmd.AddValue ("Distance", "The distance between two phys", input.distance);
-  cmd.AddValue ("PacketSize", "The size of each packet sent", input.packetSize);
-  cmd.AddValue ("TxMode", "The mode to use to send each packet", input.txMode);
-  cmd.AddValue ("NPackets", "The number of packets to send", input.nPackets);
-  cmd.AddValue ("TxPowerLevel", "The power level index to use to send each packet", input.txPowerLevel);
-  cmd.Parse (argc, argv);
-
-  struct PsrExperiment::Output output;
-  output = experiment.Run (input);
-
-  double psr = output.received;
-  psr /= input.nPackets;
-
-  std::cout << psr << std::endl;
-}
-
-double CalcPsr (struct PsrExperiment::Output output, struct PsrExperiment::Input input)
-{
-  double psr = output.received;
-  psr /= input.nPackets;
-  return psr;
-}
-
-static void PrintPsrVsDistance (int argc, char *argv[])
-{
-  struct PsrExperiment::Input input;
-  CommandLine cmd;
-  cmd.AddValue ("TxPowerLevel", "The power level index to use to send each packet", input.txPowerLevel);
-  cmd.AddValue ("TxMode", "The mode to use to send each packet", input.txMode);
-  cmd.AddValue ("NPackets", "The number of packets to send", input.nPackets);
-  cmd.AddValue ("PacketSize", "The size of each packet sent", input.packetSize);
-  cmd.Parse (argc, argv);
-  for (input.distance = 1.0; input.distance < 165; input.distance += 2.0)
-    {
-      std::cout << input.distance;
-      PsrExperiment experiment;
-      struct PsrExperiment::Output output;
-
-      input.txMode = "OfdmRate6Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate9Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate12Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate18Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate24Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate36Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate48Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      input.txMode = "OfdmRate54Mbps";
-      output = experiment.Run (input);
-      std::cout << " " << CalcPsr (output, input);
-
-      std::cout << std::endl;
-    }
-}
-
-static void PrintSizeVsRange (int argc, char *argv[])
-{
-  double targetPsr = 0.05;
-  struct PsrExperiment::Input input;
-  CommandLine cmd;
-  cmd.AddValue ("TxPowerLevel", "The power level index to use to send each packet", input.txPowerLevel);
-  cmd.AddValue ("TxMode", "The mode to use to send each packet", input.txMode);
-  cmd.AddValue ("NPackets", "The number of packets to send", input.nPackets);
-  cmd.AddValue ("TargetPsr", "The psr needed to assume that we are within range", targetPsr);
-  cmd.Parse (argc, argv);
-  for (input.packetSize = 10; input.packetSize < 3000; input.packetSize += 40)
-    {
-      double precision = 0.1;
-      double low = 1.0;
-      double high = 200.0;
-      while (high - low > precision)
-        {
-          double middle = low + (high - low) / 2;
-          struct PsrExperiment::Output output;
-          PsrExperiment experiment;
-          input.distance = middle;
-          output = experiment.Run (input);
-          double psr = CalcPsr (output, input);
-          if (psr >= targetPsr)
-            {
-              low = middle;
-            }
-          else
-            {
-              high = middle;
-            }
-        }
-      std::cout << input.packetSize << " " << input.distance << std::endl;
-    }
-}
-
-static void PrintPsrVsCollisionInterval (int argc, char *argv[])
+static void Run(int argc, char *argv[])
 {
   CollisionExperiment::Input input;
   input.nPackets = 100;
@@ -422,36 +215,6 @@ static void PrintPsrVsCollisionInterval (int argc, char *argv[])
 
 int main (int argc, char *argv[])
 {
-  if (argc <= 1)
-    {
-      std::cout << "Available experiments: "
-                << "Psr "
-                << "SizeVsRange "
-                << "PsrVsDistance "
-                << "PsrVsCollisionInterval "
-                << std::endl;
-      return 0;
-    }
-  std::string type = argv[1];
-  argc--;
-  argv[1] = argv[0];
-  argv++;
-  if (type == "Psr")
-    {
-      PrintPsr (argc, argv);
-    }
-  else if (type == "SizeVsRange")
-    {
-      PrintSizeVsRange (argc, argv);
-    }
-  else if (type == "PsrVsDistance")
-    {
-      PrintPsrVsDistance (argc, argv);
-    }
-  else if (type == "PsrVsCollisionInterval")
-    {
-      PrintPsrVsCollisionInterval (argc, argv);
-    }
-
+    Run (argc, argv);
   return 0;
 }
