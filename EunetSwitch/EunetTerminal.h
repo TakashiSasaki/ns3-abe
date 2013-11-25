e0f30144806be1c1606d296a1d68e0597a486d04
Takashi SASAKI <takashi316@gmail.com>
2013-11-25 16:41:02 +0900

    implemented PacketSinkNode

--- EunetSwitch/EunetTerminal.h
+++ EunetSwitch/EunetTerminal.h
@@ -5,13 +5,10 @@
 #include "ns3/application-container.h"
 #include "ns3/nstime.h"
 #include "ns3/ipv4-address-helper.h"
-#include "CsmaNode.h"
-#include "CsmaChannelNode.h"
-#include "CsmaInternetNode.h"
+#include "PacketSinkNode.h"
 
-class EunetTerminal: public CsmaInternetNode {
+class EunetTerminal: public PacketSinkNode {
 	ns3::ApplicationContainer onOffApplication;
-	ns3::ApplicationContainer packetSink;
 public:
 	static ns3::TypeId GetTypeId(void);
 	EunetTerminal();
@@ -21,17 +18,13 @@ public:
 	void startOnOffApplication(ns3::Time start_seconds = ns3::Seconds(0.0));
 	void stopOnOffApplication(ns3::Time stop_seconds = ns3::Seconds(10.0));
 	void setRemote(ns3::Ptr<EunetTerminal> ptr_remote);
-	uint32_t getTotalRx();
-	void logTotalRx(const ns3::LogLevel log_level = ns3::LOG_LEVEL_INFO);
 	void assignAddress(ns3::Ipv4AddressHelper&);
 protected:
 	virtual void DoInitialize();
 	virtual void NotifyConstructionCompleted();
 private:
-	static const int PACKET_SINK_UDP_PORT = 9; // Discard port (RFC 863)
-	static const int ON_OFF_APPLICATION_UDP_PORT = 10009;
-	void installPacketSink();
 	void installOnOffApplication();
+	static const int ON_OFF_APPLICATION_UDP_PORT = 10009;
 };//EunetTerminal
 
 #endif /* EUNETTERMINAL_H_ */
