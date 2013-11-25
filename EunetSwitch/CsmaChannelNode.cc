701653df785d111ceec521f1005bc2da839a5b91
Takashi SASAKI <takashi316@gmail.com>
2013-11-25 14:13:20 +0900

    implementing CsmaChannelNode

---
+++ EunetSwitch/CsmaChannelNode.cc
@@ -0,0 +1,26 @@
+#define NS3_LOG_ENABLE 1
+#include "ns3/log.h"
+NS_LOG_COMPONENT_DEFINE("CsmaChannelNode");
+#define NS3_ASSERT_ENABLE 1
+#include "ns3/assert.h"
+#include "ns3/queue.h"
+#include "ns3/node.h"
+#include "CsmaNode.h"
+#include "CsmaChannelNode.h"
+NS_OBJECT_ENSURE_REGISTERED(CsmaChannelNode);
+
+ns3::TypeId CsmaChannelNode::GetTypeId(void) {
+	static ns3::TypeId type_id = ns3::TypeId("CsmaChannelNode").SetParent<
+			CsmaNode> ().AddConstructor<CsmaChannelNode> ();
+	return type_id;
+}//GetTypeId
+
+CsmaChannelNode::CsmaChannelNode() {
+	this->csmaChannelFactory.SetTypeId("ns3::CsmaChannel");
+	this->ptrCsmaChannel = this->csmaChannelFactory.Create()->GetObject<
+			ns3::CsmaChannel> ();
+}
+
+CsmaChannelNode::~CsmaChannelNode() {
+	// TODO !CodeTemplates.destructorstub.tododesc!
+}
---
+++ EunetSwitch/CsmaChannelNode.h
@@ -0,0 +1,17 @@
+#ifndef CSMACHANNELNODE_H_
+#define CSMACHANNELNODE_H_
+#include "ns3/node.h"
+#include "CsmaNode.h"
+#include "ns3/csma-channel.h"
+
+class CsmaChannelNode: public CsmaNode {
+	ns3::Ptr<ns3::CsmaChannel> ptrCsmaChannel;
+	ns3::ObjectFactory csmaChannelFactory;
+
+public:
+	static ns3::TypeId GetTypeId(void);
+	CsmaChannelNode();
+	virtual ~CsmaChannelNode();
+};
+
+#endif /* CSMACHANNELNODE_H_ */
