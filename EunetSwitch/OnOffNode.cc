02bb61a8a4bab6e2dde98a7a30de15a88c1ca38b
Takashi SASAKI <takashi316@gmail.com>
2013-11-26 21:25:41 +0900

    OnOffNode does not start OnOffApplication at the start of simulation implicitly

--- EunetSwitch/OnOffNode.cc
+++ EunetSwitch/OnOffNode.cc
@@ -5,7 +5,7 @@ NS_LOG_COMPONENT_DEFINE("OnOffNode");
 #include "ns3/assert.h"
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-address-helper.h"
-
+#include "ns3/nstime.h"
 #include "OnOffNode.h"
 NS_OBJECT_ENSURE_REGISTERED(OnOffNode);
 
@@ -33,11 +33,14 @@ void OnOffNode::NotifyConstructionCompleted() {
 	NS_LOG_INFO("notified the completion of OnOffNode");
 	NS_ASSERT(this->GetNDevices() == 1);
 	Base::NotifyConstructionCompleted();
-	NS_ASSERT(this->GetNDevices() == 2); NS_LOG_INFO("installing on-off application on node " << this->GetId());
+	NS_ASSERT(this->GetNDevices() == 2);NS_LOG_INFO("installing on-off application on node " << this->GetId());
 	ns3::OnOffHelper on_off_helper("ns3::UdpSocketFactory", ns3::Address(
 			ns3::InetSocketAddress(ns3::Ipv4Address("1.2.3.4"),
 					PACKET_SINK_UDP_PORT)));
 	on_off_helper.SetConstantRate(ns3::DataRate("500kb/s"));
+	on_off_helper.SetAttribute("StartTime", ns3::TimeValue(ns3::Time(
+			std::numeric_limits<int64_t>::max())));
+	on_off_helper.SetAttribute("StopTime", ns3::TimeValue(ns3::Seconds(0.0)));
 	//on_off_helper.SetAttribute("Remote", ns3::AddressValue(
 	//		ns3::InetSocketAddress(ns3::Ipv4Address("127.0.0.1"),
 	//				PACKET_SINK_UDP_PORT)));
