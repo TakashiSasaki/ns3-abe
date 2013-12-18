e981a179c8d7fe7742102eb9bd87d588ad11b395
Takashi SASAKI <takashi316@gmail.com>
2013-12-18 22:19:37 +0900

    skelton

---
+++ EunetSwitch/BridgingAp.cc
@@ -0,0 +1,32 @@
+#define NS3_LOG_ENABLE 1
+#include "ns3/log.h"
+NS_LOG_COMPONENT_DEFINE("BridgingAp");
+#define NS3_ASSERT_ENABLE 1
+#include "ns3/assert.h"
+#include "ns3/wifi-helper.h"
+#include "ns3/nqos-wifi-mac-helper.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/boolean.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/ap-wifi-mac.h"
+#include "ns3/wifi-net-device.h"
+#include "SimpleAp.h"
+#include "BridgingAp.h"
+NS_OBJECT_ENSURE_REGISTERED(BridgingAp);
+
+ns3::TypeId SimpleAp::GetTypeId(void) {
+	static ns3::TypeId type_id =
+			ns3::TypeId("BridgingAp").SetParent<SimpleAp> ().AddConstructor<
+					BridgingAp> ();
+	return type_id;
+}//GetTypeId
+
+BridgingAp::BridgingAp() {
+	// TODO 自動生成されたコンストラクター・スタブ
+
+}
+
+BridgingAp::~BridgingAp() {
+	// TODO !CodeTemplates.destructorstub.tododesc!
+}
---
+++ EunetSwitch/BridgingAp.h
@@ -0,0 +1,12 @@
+#ifndef BRIDGINGAP_H_
+#define BRIDGINGAP_H_
+
+#include "SimpleAp.h"
+
+class BridgingAp: public SimpleAp {
+public:
+	BridgingAp();
+	virtual ~BridgingAp();
+};
+
+#endif /* BRIDGINGAP_H_ */
