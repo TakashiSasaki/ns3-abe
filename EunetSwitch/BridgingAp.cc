2fdd9ecc4947e3338f64f540dd3bab0133a5f36d
Takashi SASAKI <takashi316@gmail.com>
2013-12-18 22:28:21 +0900

    bridging CsmaNetDevice and WifiNetDevice

--- EunetSwitch/BridgingAp.cc
+++ EunetSwitch/BridgingAp.cc
@@ -11,6 +11,7 @@ NS_LOG_COMPONENT_DEFINE("BridgingAp");
 #include "ns3/mobility-helper.h"
 #include "ns3/ap-wifi-mac.h"
 #include "ns3/wifi-net-device.h"
+#include "ns3/bridge-net-device.h"
 #include "SimpleAp.h"
 #include "BridgingAp.h"
 NS_OBJECT_ENSURE_REGISTERED(BridgingAp);
@@ -37,6 +38,13 @@ void SimpleAp::DoInitialize() {
 }
 
 void BridgingAp::NotifyConstructionCompleted() {
+	ns3::Ptr<ns3::BridgeNetDevice> ptr_bridge_net_device = this->getNetDevice<
+			ns3::BridgeNetDevice> ();
+	NS_ASSERT(ptr_bridge_net_device != 0);
+	ns3::Ptr<ns3::WifiNetDevice> ptr_wifi_net_device = this->getNetDevice<
+			ns3::WifiNetDevice> ();
+	NS_ASSERT(ptr_wifi_net_device != 0);
+	ptr_bridge_net_device->AddBridgePort(ptr_wifi_net_device);
 	NS_LOG_INFO("just calling up SimpleAp::NotifyConstructionCompleted");
 	Base::NotifyConstructionCompleted();
 }
