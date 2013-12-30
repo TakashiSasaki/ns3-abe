fe4709de948707a9915344175b20b437a40420b5
Takashi SASAKI <takashi316@gmail.com>
2013-12-30 13:28:05 +0900

    correct log component name in EunetRouter. Note that this module does not work. Just to pass build process.

--- EunetSwitch/model/EunetRouter.cc
+++ EunetSwitch/model/EunetRouter.cc
@@ -2,7 +2,7 @@
 #include <sstream>
 #define NS3_LOG_ENABLE 1
 #include "ns3/log.h"
-NS_LOG_COMPONENT_DEFINE("SimpleRouter");
+NS_LOG_COMPONENT_DEFINE("EunetRouter");
 #define NS3_ASSERT_ENABLE 1
 #include "ns3/assert.h"
 #include "ns3/type-id.h"
