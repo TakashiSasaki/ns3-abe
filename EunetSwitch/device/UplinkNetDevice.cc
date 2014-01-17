542c6bd4fd109387701ea7c5a54b66ca50d6a3f6
Takashi SASAKI <takashi316@gmail.com>
2014-01-18 00:15:44 +0900

    type id bug fix

--- EunetSwitch/device/UplinkNetDevice.cc
+++ EunetSwitch/device/UplinkNetDevice.cc
@@ -8,7 +8,7 @@ NS_LOG_COMPONENT_DEFINE("UplinkNetDevice");
 NS_OBJECT_ENSURE_REGISTERED(UplinkNetDevice);
 
 ns3::TypeId UplinkNetDevice::GetTypeId(void) {
-	static ns3::TypeId type_id = ns3::TypeId("CsmaChannelNode"). SetParent<
+	static ns3::TypeId type_id = ns3::TypeId("UplinkNetDevice"). SetParent<
 			ns3::CsmaNetDevice> () .AddConstructor<UplinkNetDevice> ();
 	return type_id;
 }//GetTypeId
