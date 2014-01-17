8c768ced684d3ed565424668b885bfdcb33f7656
Takashi SASAKI <takashi316@gmail.com>
2014-01-17 18:53:29 +0900

    reoved unused lines

--- EunetSwitch/test/SimpleApTest.cc
+++ EunetSwitch/test/SimpleApTest.cc
@@ -1,6 +1,8 @@
 #define NS3_LOG_ENABLE 1
 #include "ns3/log.h"
 NS_LOG_COMPONENT_DEFINE("SimpleApTest");
+#define NS3_ASSERT_ENABLE 1
+#include "ns3/assert.h"
 #include "ns3/test.h"
 #include "ns3/simulator.h"
 #include "SimpleAp.h"
@@ -9,7 +11,6 @@ class SimpleApTestCase: public ns3::TestCase {
 public:
 	SimpleApTestCase() :
 		ns3::TestCase("SimpleApTestCase") {
-		//NS_LOG_UNCOND("constructing a test case");
 	}
 	virtual ~SimpleApTestCase() {
 	}
@@ -20,7 +21,7 @@ private:
 		object_factory.SetTypeId("SimpleAp");
 		auto simple_ap = object_factory.Create<SimpleAp> ();
 		simple_ap->setSsid(ns3::Ssid("eunet"));
-		auto simple_ap_2 = object_factory.Create<SimpleAp>();
+		auto simple_ap_2 = object_factory.Create<SimpleAp> ();
 		simple_ap_2->setSsid(ns3::Ssid("eunet"));
 		NS_LOG_INFO("Run Simulation.");
 		ns3::Simulator::Stop(ns3::Seconds(0.1));
@@ -34,10 +35,8 @@ class SimpleApTestSuite: public ns3::TestSuite {
 public:
 	SimpleApTestSuite() :
 		ns3::TestSuite("SimpleAp", UNIT) {
-		//NS_LOG_UNCOND("adding a test case");
 		AddTestCase(new SimpleApTestCase, ns3::TestCase::QUICK);
 	}
 };
 
 static SimpleApTestSuite simple_ap_test_suite;
-
