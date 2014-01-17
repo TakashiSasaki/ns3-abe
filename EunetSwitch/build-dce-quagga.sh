4da5bfc9985f0f13929342fb843dd2a9164d050d
Takashi SASAKI <takashi316@gmail.com>
2014-01-17 13:51:18 +0900

    gccxml-ns3 and pygccsml are to be built

--- EunetSwitch/build-dce-quagga.sh
+++ EunetSwitch/build-dce-quagga.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-hg clone http://code.nsnam.org/bake bake
+if [ -e bake ]; then
+  hg pull bake
+else
+  hg clone http://code.nsnam.org/bake bake
+fi
 export BAKE_HOME=`pwd`/bake
 export PATH=$PATH:$BAKE_HOME
 export PYTHONPATH=$PYTHONPATH:$BAKE_HOME
 mkdir dce
 cd dce
-bake.py configure -vvv -e dce-ns3-dev -e dce-quagga-dev
+#bake.py distclean
+#bake.py check  --all
+bake.py configure -vvv -e dce-ns3-dev -e dce-quagga-dev -e gccxml-ns3 -e pygccxml
+#(cd dce/source/ns-3-dev-dce/; ./waf distclean)
+#bake.py configure -vvv -e dce-ns3-dev -e dce-quagga-dev -e gccxml-ns3 -e pygccxml
+bake.py check -v
 bake.py download -vvv
-bake.py build -vvv
+bake.py build -vvv 
 
