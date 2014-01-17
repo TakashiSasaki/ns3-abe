2cce901f651d67dc3ce023b1a2c312a4486fcf9e
Takashi SASAKI <takashi316@gmail.com>
2014-01-17 21:36:45 +0900

    added attributes

--- EunetSwitch/device/UplinkNetDevice.h
+++ EunetSwitch/device/UplinkNetDevice.h
@@ -2,7 +2,9 @@
 #define UPLINKNETDEVICE_H_
 #include "ns3/csma-net-device.h"
 
-class UplinkNetDevice : public ns3::CsmaNetDevice{
+class UplinkNetDevice: public ns3::CsmaNetDevice {
+	ns3::DataRate uplinkChannelDataRate;
+	ns3::Time uplinkChannelDelay;
 public:
 	static ns3::TypeId GetTypeId(void);
 	UplinkNetDevice();
