c27aee92ed2d80108b43e0c7627cb2068683185d
Takashi SASAKI <takashi316@gmail.com>
2014-01-17 21:35:08 +0900

    added GetTypeId

--- EunetSwitch/device/UplinkNetDevice.cc
+++ EunetSwitch/device/UplinkNetDevice.cc
@@ -3,8 +3,15 @@
 NS_LOG_COMPONENT_DEFINE("UplinkNetDevice");
 #define NS3_ASSERT_ENABLE 1
 #include "ns3/assert.h"
+#include "ns3/csma-net-device.h"
 #include "UplinkNetDevice.h"
 NS_OBJECT_ENSURE_REGISTERED(UplinkNetDevice);
+
+ns3::TypeId UplinkNetDevice::GetTypeId(void) {
+	static ns3::TypeId type_id = ns3::TypeId("CsmaChannelNode"). SetParent<
+			ns3::CsmaNetDevice> () .AddConstructor<UplinkNetDevice> ();
+	return type_id;
+}//GetTypeId
 
 UplinkNetDevice::UplinkNetDevice() {
 }
