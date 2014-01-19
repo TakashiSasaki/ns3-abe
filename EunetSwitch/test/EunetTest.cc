6473bb609d19033642e5cbf5910416a03c3d93a9
Takashi SASAKI <takashi316@gmail.com>
2014-01-20 04:46:05 +0900

    descriptive picture as comment

--- EunetSwitch/test/EunetTest.cc
+++ EunetSwitch/test/EunetTest.cc
@@ -27,6 +27,13 @@ void EunetTestCase::DoRun() {
 	const ns3::DataRate DATARATE_10GBPS("10Gbps");
 	const ns3::DataRate DATARATE_1GBPS("1Gbps");
 
+	/*
+	 * configuring network 1 with three switches
+	 *
+	 *   s12---s11---s13
+	 *
+	 */
+
 	// creating three switches for network 1
 	auto ptr_s11 = eunet.addEunetSwitch("s11", 4, 24, DATARATE_10GBPS,
 			DATARATE_1GBPS);
@@ -44,12 +51,28 @@ void EunetTestCase::DoRun() {
 			ns3::Ipv4Address("192.168.1.0"), ns3::Ipv4Mask("255.255.255.0"));
 	ptr_s11->eunetTerminals.assignAddresses(ipv4_address_helper_1);
 	ptr_s12->eunetTerminals.assignAddresses(ipv4_address_helper_1);
+	ptr_s13->eunetTerminals.assignAddresses(ipv4_address_helper_1);
 
-	// setting destination address for OnOffApplication on s11/terminal3 to s12/terminal5
-	auto t113 = ptr_s11->eunetTerminals.Get(3)->GetObject<EunetTerminal> ();
+	/*
+	 * setting destination address for OnOffApplication on s11/terminal3 to s12/terminal5
+	 *
+	 *   s12---s11---s13
+	 *     |             |
+	 *   t125        t133
+	 * PacketSink  OnOffApplication
+	 */
+	auto t133 = ptr_s13->eunetTerminals.Get(3)->GetObject<EunetTerminal> ();
 	auto t125 = ptr_s12->eunetTerminals.Get(5)->GetObject<EunetTerminal> ();
-	t113->setRemote(t125->getAddress<ns3::CsmaNetDevice> (0));
+	t133->setRemote(t125->getAddress<ns3::CsmaNetDevice> (0));
 
+	/*
+	 * configuring network 2 with three switches
+	 *
+	 *   s22---s21---s23
+	 *     |             |
+	 *   t225        t233
+	 * PacketSink  OnOffApplication
+	 */
 	auto ptr_s21 = eunet.addEunetSwitch("s21", 4, 24, DATARATE_10GBPS,
 			DATARATE_1GBPS);
 	auto ptr_s22 = eunet.addEunetSwitch("s22", 4, 24, DATARATE_10GBPS,
@@ -64,6 +87,11 @@ void EunetTestCase::DoRun() {
 			ns3::Ipv4Address("192.168.2.0"), ns3::Ipv4Mask("255.255.255.0"));
 	ptr_s21->eunetTerminals.assignAddresses(ipv4_address_helper_2);
 	ptr_s22->eunetTerminals.assignAddresses(ipv4_address_helper_2);
+	ptr_s23->eunetTerminals.assignAddresses(ipv4_address_helper_2);
+
+	auto t233 = ptr_s23->eunetTerminals.Get(3)->GetObject<EunetTerminal> ();
+	auto t225 = ptr_s22->eunetTerminals.Get(5)->GetObject<EunetTerminal> ();
+	t233->setRemote(t225->getAddress<ns3::CsmaNetDevice> (0));
 
 	// attaching corresponding CSMA channel to EunetTerminals
 	eunet.attachEunetTerminals();
@@ -71,8 +99,10 @@ void EunetTestCase::DoRun() {
 	Simulator::Stop(Seconds(this->stopTime));
 	Simulator::Run();
 	Simulator::Destroy();
-	NS_ASSERT_MSG(t113->getTotalRx() == 0, t113->getTotalRx());
-	NS_ASSERT_MSG(t125->getTotalRx() == 342016, t125->getTotalRx());
+	NS_ASSERT_MSG(t133->getTotalRx() == 0, t133->getTotalRx());
+	NS_ASSERT_MSG(t125->getTotalRx() == 340480, t225->getTotalRx());
+	NS_ASSERT_MSG(t233->getTotalRx() == 0, t133->getTotalRx());
+	NS_ASSERT_MSG(t225->getTotalRx() == 340992, t225->getTotalRx());
 }
 
 class EunetTestSuite: public TestSuite {
