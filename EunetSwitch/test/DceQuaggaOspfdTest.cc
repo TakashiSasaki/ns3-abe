88df1bf3127c0a55c02175125c782edb9ec67624
Takashi SASAKI <takashi316@gmail.com>
2014-01-27 20:03:19 +0900

    installing Ipv4DceRoutingHelper after installing IP protocol stack

--- EunetSwitch/test/DceQuaggaOspfdTest.cc
+++ EunetSwitch/test/DceQuaggaOspfdTest.cc
@@ -209,6 +209,101 @@ void DceQuaggaOspfdSeparatedTestCase::DoRun() {
 	Simulator::Destroy();
 }//DceQuaggaOspfdSeparatedTestCase::DoRun
 
+class DceQuaggaOspfdSetRoutingProtocolTestCase: public TestCase {
+public:
+	DceQuaggaOspfdSetRoutingProtocolTestCase() :
+		ns3::TestCase("DceQuaggaOspfdSetRoutingProtocolTestCase") {
+	}
+	virtual ~DceQuaggaOspfdSetRoutingProtocolTestCase() {
+	}
+
+private:
+	virtual void DoRun(void);
+}; //class DceQuaggaOspfdSetRoutingProtocolTestCase
+
+void DceQuaggaOspfdSetRoutingProtocolTestCase::DoRun() {
+	NodeContainer nodes;
+	nodes.Create(nNodes);
+
+	CsmaHelper csma_helper;
+	csma_helper.SetChannelAttribute("DataRate", StringValue("5Mbps"));
+	csma_helper.SetChannelAttribute("Delay", StringValue("2ms"));
+
+	PointToPointHelper pointToPoint;
+	pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+	pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+	NetDeviceContainer devices;
+	devices = csma_helper.Install(nodes);
+	auto ndc_point_to_point = pointToPoint.Install(nodes);
+
+	{
+		// Internet stack install
+		InternetStackHelper stack1; // IPv4 is required for GlobalRouteMan
+		//Ipv4DceRoutingHelper ipv4RoutingHelper1;
+		//stack1.SetRoutingHelper(ipv4RoutingHelper1);
+		stack1.Install(nodes.Get(0));
+
+		InternetStackHelper stack2; // IPv4 is required for GlobalRouteMan
+		//Ipv4DceRoutingHelper ipv4RoutingHelper2;
+		//stack2.SetRoutingHelper(ipv4RoutingHelper2);
+		stack2.Install(nodes.Get(1));
+
+	}
+
+	{
+		auto ipv4_0 = nodes.Get(0)->GetObject<ns3::Ipv4> ();
+		Ipv4DceRoutingHelper ipv4_routing_helper_0;
+		auto ipv4_routing_protocol_0 = ipv4_routing_helper_0.Create(
+				nodes.Get(0));
+		ipv4_0->SetRoutingProtocol(ipv4_routing_protocol_0);
+
+		auto ipv4_1 = nodes.Get(1)->GetObject<ns3::Ipv4> ();
+		Ipv4DceRoutingHelper ipv4_routing_helper_1;
+		auto ipv4_routing_protocol_1 = ipv4_routing_helper_1.Create(
+				nodes.Get(1));
+		ipv4_1->SetRoutingProtocol(ipv4_routing_protocol_1);
+	}
+
+	{
+		// assigning IP address
+		Ipv4AddressHelper ipv4AddrHelper;
+		ipv4AddrHelper.SetBase("10.0.0.0", "255.255.255.0");
+		Ipv4InterfaceContainer interfaces = ipv4AddrHelper.Assign(devices);
+		Ipv4InterfaceContainer iic_point_to_point = ipv4AddrHelper.Assign(
+				ndc_point_to_point);
+		//Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+	}
+
+	DceManagerHelper processManager1;
+	processManager1.SetNetworkStack("ns3::Ns3SocketFdFactory");
+	processManager1.Install(nodes.Get(0));
+
+	QuaggaHelper quagga1;
+	quagga1.EnableOspf(nodes.Get(0), "10.0.0.0/24");
+	quagga1.EnableOspfDebug(nodes.Get(0));
+	quagga1.EnableZebraDebug(nodes.Get(0));
+	quagga1.Install(nodes.Get(0));
+
+	DceManagerHelper processManager2;
+	processManager2.SetNetworkStack("ns3::Ns3SocketFdFactory");
+	processManager2.Install(nodes.Get(1));
+
+	QuaggaHelper quagga2;
+	quagga2.EnableOspf(nodes.Get(1), "10.0.0.0/24");
+	quagga2.EnableOspfDebug(nodes.Get(1));
+	quagga2.EnableZebraDebug(nodes.Get(1));
+	quagga2.Install(nodes.Get(1));
+
+	pointToPoint.EnablePcapAll("DceQuaggaOspfdSetRoutingProtocolTestCase");
+	csma_helper.EnablePcapAll("DceQuaggaOspfdSetRoutingProtocolTestCase");
+
+	if (stopTime != 0) {
+		Simulator::Stop(Seconds(stopTime));
+	}
+	Simulator::Run();
+	Simulator::Destroy();
+}//DceQuaggaOspfdSetRoutingProtocolTestCase::DoRun
 
 class DceQuaggaOspfdTestSuite: public TestSuite {
 public:
@@ -217,6 +312,8 @@ public:
 		//NS_LOG_UNCOND("adding a test case");
 		AddTestCase(new DceQuaggaOspfdTestCase, TestCase::QUICK);
 		AddTestCase(new DceQuaggaOspfdSeparatedTestCase, TestCase::QUICK);
+		AddTestCase(new DceQuaggaOspfdSetRoutingProtocolTestCase,
+				TestCase::QUICK);
 	}
 };
 
