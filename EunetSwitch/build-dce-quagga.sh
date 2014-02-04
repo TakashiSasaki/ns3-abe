d996fa36e3101df771beabaad09572aaf4b4db64
Takashi SASAKI <takashi316@gmail.com>
2014-02-04 22:17:40 +0900

    visualization enabled

--- EunetSwitch/build-dce-quagga.sh
+++ EunetSwitch/build-dce-quagga.sh
@@ -10,11 +10,13 @@ export PYTHONPATH=$PYTHONPATH:$BAKE_HOME
 mkdir dce
 cd dce
 #bake.py distclean
-#bake.py check  --all
-bake.py configure -vvv -e dce-ns3-dev -e dce-quagga-dev -e gccxml-ns3 -e pygccxml
+bake.py check  --all
+bake.py show --available
+bake.py configure -vvv -e dce-ns3-dev -e dce-quagga-dev -e gccxml-ns3 -e pygccxml -e pybindgen-0.16.0.834
+bake.py deploy
+bake.py check -v
 #(cd dce/source/ns-3-dev-dce/; ./waf distclean)
 #bake.py configure -vvv -e dce-ns3-dev -e dce-quagga-dev -e gccxml-ns3 -e pygccxml
-bake.py check -v
 bake.py download -vvv
 bake.py build -vvv 
 
